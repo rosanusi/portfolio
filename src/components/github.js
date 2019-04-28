94c043550f8a9b21067833f2df729c951dd4d1dc
Ridwan Sanusi <ridwan.sanusi@booking.com>
2019-04-29 01:17:43 +0200

    Added new side project - Quran for curious mind

--- src/components/github.js
+++ src/components/github.js
@@ -8,6 +8,9 @@ export default class github extends Component {
             <h6 className="section-title">Active side projects</h6>
     
             <ul className="rdwn-github-projects">
+            <li>
+                    <a href="https://quran-curious-mind.netlify.com/" target="_blank" rel="noopener noreferrer">Quran for the curious mind</a>
+                </li>
                 <li>
                     <a href="https://github.com/rosanusi/project_journal" target="_blank" rel="noopener noreferrer">Project Journal</a>
                 </li>
