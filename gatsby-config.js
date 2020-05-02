592635e439ca35778cbdee088580fb4e5b7b793e
Ridwan Sanusi <ridwan.sanusi@booking.com>
2020-05-02 11:29:58 +0200

    cant get purgecss to to work for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,14 +68,6 @@ module.exports = {
         trackingId: "UA-136403700-1",
       },
     },
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: true,
-        develop: true,
-        tailwind: true
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
